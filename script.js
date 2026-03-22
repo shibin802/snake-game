12d0eced1931db5aed55196cde56981f4ef45832
root <root@localhost.localdomain>
2026-03-22 08:04:46 +0800

    feat: disable mobile zoom gestures in snake game

--- script.js
+++ script.js
@@ -311,3 +311,17 @@ pauseBtn.addEventListener('click', togglePause);
 restartBtn.addEventListener('click', resetGame);
 
 resetGame();
+
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', (event) => {
+  const now = Date.now();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, { passive: false });
+
+document.addEventListener('gesturestart', (event) => {
+  event.preventDefault();
+});
